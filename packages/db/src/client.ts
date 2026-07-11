b4379959a2f6c1fd9c886ef89bb1a508afe3f053
bimoadis <bimoadi310@gmail.com>
2026-07-11 10:29:44 +0700

    fix: place DNS IPv4 priority configuration directly in db client module to prevent ESM hoisting race conditions

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -2,6 +2,9 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import pg from 'pg';
 import * as schema from './schema.js';
 import dotenv from 'dotenv';
+import dns from 'dns';
+
+dns.setDefaultResultOrder('ipv4first');
 
 dotenv.config();
 dotenv.config({ path: '../../.env' });
